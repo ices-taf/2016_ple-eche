f9e5b89b3efe781a85002024e832c02dfa322b5b
Arni Magnusson <arnima@hafro.is>
2017-09-21 13:17:26 -0400

    Remove all.R and clean.R

--- all.R
+++
@@ -1,9 +0,0 @@
-## Run core scripts: db, input, model, output, upload
-
-library(icesTAF)
-
-sourceTAF("db.R")
-sourceTAF("input.R")
-sourceTAF("model.R")
-sourceTAF("output.R")
-sourceTAF("upload.R")
