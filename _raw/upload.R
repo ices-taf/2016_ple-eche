af9fbb2113df819da2e18bba7b965120747037de
Arni Magnusson <arnima@hafro.is>
2017-09-26 15:35:41 +0200

    Upload stockobject.Rdata & PLE7DFleet_2016.txt

--- _raw/upload.R
+++ _raw/upload.R
@@ -6,5 +6,6 @@
 library(icesTAF)
 
 owd <- setwd("d:/projects/ices-taf/ftp/wgnssk/2016/ple-eche/raw")
-upload("2016_ple-eche", "raw", "catageysa.dat")
+upload("2016_ple-eche", "raw", "stockobject.Rdata")
+upload("2016_ple-eche", "raw", "PLE7DFleet_2016.txt")
 setwd(owd)
