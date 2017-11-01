4dec2412914fd65ba636e333ee6f94f63f800b69
Arni Magnusson <arnima@hafro.is>
2017-11-01 06:54:18 +0100

    Update header comment

--- report_plots.R
+++ report_plots.R
@@ -1,7 +1,7 @@
 ## Prepare plots for report
 
 ## Before: summary.csv (output)
-## After:  biomass.png, catches.png, f_mortality.png, recruitment.png (report)
+## After:  biomass.png, f_mortality.png (report)
 
 library(icesTAF)
 library(areaplot)
@@ -13,14 +13,14 @@ summary <- read.taf("output/summary.csv")
 x <- div(summary, "SSB", grep=TRUE)
 
 ## Plots
-tafpng("f_mortality")
-confplot(x[c("Year","Fbar_lo","Fbar_hi")], ylim=lim(x$Fbar_hi,1.3), yaxs="i",
-         ylab="Fbar (3-6)", main="Fishing mortality")
-lines(x$Year, x$Fbar)
-dev.off()
-
 tafpng("biomass")
 confplot(x[c("Year","SSB_lo","SSB_hi")], ylim=lim(x$SSB_hi), yaxs="i",
          ylab="SSB (1000 t)", main="Biomass")
 lines(x$Year, x$SSB)
+dev.off()
+
+tafpng("f_mortality")
+confplot(x[c("Year","Fbar_lo","Fbar_hi")], ylim=lim(x$Fbar_hi,1.3), yaxs="i",
+         ylab="Fbar (3-6)", main="Fishing mortality")
+lines(x$Year, x$Fbar)
 dev.off()
