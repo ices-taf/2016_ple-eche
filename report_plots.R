31deab08deaafdf7aa28d16f539f112b94fd08ea
Arni Magnusson <arnima@hafro.is>
2018-12-09 16:14:47 +0100

    icesTAF 2.0-0 notation: taf.png() instead of tafpng()

--- report_plots.R
+++ report_plots.R
@@ -13,13 +13,13 @@ summary <- read.taf("output/summary.csv")
 x <- div(summary, "SSB", grep=TRUE)
 
 ## Plots
-tafpng("biomass")
+taf.png("biomass")
 confplot(x[c("Year","SSB_lo","SSB_hi")], ylim=lim(x$SSB_hi), yaxs="i",
          ylab="SSB (1000 t)", main="Biomass")
 lines(x$Year, x$SSB)
 dev.off()
 
-tafpng("f_mortality")
+taf.png("f_mortality")
 confplot(x[c("Year","Fbar_lo","Fbar_hi")], ylim=lim(x$Fbar_hi), yaxs="i",
          ylab="Fbar (3-6)", main="Fishing mortality")
 lines(x$Year, x$Fbar)
